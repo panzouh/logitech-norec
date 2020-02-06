0b6dd19b76389c1b8e360160ab6b7042d8cf0e65
13paulmurith <13paulmurith@gmail.com>
2020-02-06 09:15:08 +0100

    Update arma-norec-v3.lua

--- arma-norec-v3.lua
+++ arma-norec-v3.lua
@@ -1,158 +1,9 @@
-local Zafir_Pattern = { 
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.3, y = 1 },
-    { x = -0.3, y = 1 },
-    { x = -0.3, y = 1 },
-    { x = -0.2, y = 1 },
-    { x = -0.3, y = 1 },
-    { x = -0.3, y = 1 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.3, y = 1.2 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.4, y = 1.3 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.5, y = 1.4 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.6, y = 1.5 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.7, y = 1.6 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-    { x = -0.8, y = 1.7 },
-}
 local Activation_Key = 4
 local Button = false
-local Randomize = math.random(47,48)
+local CorrectionLVL = 0
+local Spray = 0
+local Pattern = {-0.8, 1.5, -0.9, 2, -1, 2.5}
+local RandomizeSleep = math.random(47,48)
 
 EnablePrimaryMouseButtonEvents(true);
 
@@ -163,17 +14,35 @@ end
 function OnEvent(event, arg)
     if IsMouseButtonPressed(Activation_Key) and Button == false then
         OutputLogs('\nHello there')
+        OutputLogs('\nCorrectionLVL => 1')
         Button = true
     elseif IsMouseButtonPressed(Activation_Key)  and Button == true then
         OutputLogs('\nYou are a bold one')
         Button = false
     end
-    if Button == true and IsMouseButtonPressed(1) then
-        if IsMouseButtonPressed(1) then
-            for i=0, #Zafir_Pattern do
-                Sleep(Randomize)
-                MoveMouseRelative(Zafir_Pattern[i].x, Zafir_Pattern[i].y)
-            end
+
+    if Button == true then
+        if IsMouseButtonPressed(1) and arg == 1 then
+            repeat
+                MoveMouseRelative(Pattern[0],Pattern[1])
+                Sleep(RandomizeSleep)
+                Spray = Spray + 1
+                if Spray > 90 then
+                    CorrectionLVL = 1
+                    OutputLogs('\nCorrectionLVL => 2')
+                    MoveMouseRelative(Pattern[2],Pattern[3])
+                    Sleep(RandomizeSleep)
+                    if Spray == 149
+                        CorrectionLVL = 3
+                    end
+                elseif Spray > 150 and CorrectionLVL == 3
+                    OutputLogs('\nCorrectionLVL => 3')
+                    MoveMouseRelative(Pattern[4],Pattern[5])
+                    Sleep(RandomizeSleep)
+                end
+            until not IsMouseButtonPressed(1)
+            OutputLogs('\nCorrectionLVL => 1')
+            Spray = 0
         end
     end 
 end
